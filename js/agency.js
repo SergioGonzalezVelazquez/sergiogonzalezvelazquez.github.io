5fea5a82f2fe95c39d4a33b110110713f363b304
unknown <sergio.gonzalez29@alu.uclm.es>
2019-12-27 02:20:18 +0100

    Added curriculum

--- js/agency.js
+++ js/agency.js
@@ -3,6 +3,7 @@
 
   // Smooth scrolling using jQuery easing
   $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
+    console.log("entra")
     if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
       var target = $(this.hash);
       target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
